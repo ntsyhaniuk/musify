58806eb14076be3cf5ff0deb6a64e7c27d2ee550
nazartsyhanyuk <nazartsyhanyuk@gmail.com>
2020-01-02 15:32:03 +0200

    working on config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,9 @@
 export const environment = {
   production: true,
-  CLIENT_ID: '', // here should be your client id from registered application
-  REDIRECT_URI: '',
+  // @ts-ignore
+  CLIENT_ID: process.env.CLIENT_ID,
+  // @ts-ignore
+  REDIRECT_URI: process.env.REDIRECT_URI,
   BASE_URL: 'https://api.spotify.com/v1',
   STORAGE_KEY: 'auth_token',
   AUTH_URL: 'https://accounts.spotify.com/authorize'
