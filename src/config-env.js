1b53af12f0c68ec2f4bd207e9e74ad0ab8d946b0
Nazar Tsyhanyuk <Nazartsyhanyuk@gmail.com>
2020-01-02 18:49:42 +0200

    Update config-env.js

--- src/config-env.js
+++ src/config-env.js
@@ -2,13 +2,14 @@ const writeFile = require('file-system').writeFile;
 require('dotenv').config();
 
 const CLIENT_ID = process.env.CLIENT_ID;
+const REDIRECT_URI = process.env.REDIRECT_URI;
 
 const targetPath = `./src/environments/environment.prod.ts`;
 const envConfigFile = `
 export const environment = {
   production: true,
   CLIENT_ID: '${CLIENT_ID}',
-  REDIRECT_URI: 'https://musify-app.netlify.com',
+  REDIRECT_URI: '${REDIRECT_URI}',
   BASE_URL: 'https://api.spotify.com/v1',
   STORAGE_KEY: 'auth_token',
   AUTH_URL: 'https://accounts.spotify.com/authorize'
