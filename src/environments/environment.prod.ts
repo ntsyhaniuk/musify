6e4080b51f395604900b4d02f11dcf0b0a5ea446
nazartsyhanyuk <nazartsyhanyuk@gmail.com>
2020-01-02 15:41:29 +0200

    fixing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,7 @@
 export const environment = {
   production: true,
-  // @ts-ignore
-  CLIENT_ID: process.env.CLIENT_ID,
-  // @ts-ignore
-  REDIRECT_URI: process.env.REDIRECT_URI,
+  CLIENT_ID: '',
+  REDIRECT_URI: 'https://musify-app.netlify.com',
   BASE_URL: 'https://api.spotify.com/v1',
   STORAGE_KEY: 'auth_token',
   AUTH_URL: 'https://accounts.spotify.com/authorize'
