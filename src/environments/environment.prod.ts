0b99b4d4a23f1d3c1729b966498785206e1375c3
nazartsyhanyuk <nazartsyhanyuk@gmail.com>
2020-01-02 15:53:51 +0200

    fixing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,10 @@
+import { config } from 'dotenv';
+
+config({ path: '../../.env' });
+
 export const environment = {
   production: true,
-  CLIENT_ID: '',
+  CLIENT_ID: process.env.CLIENT_ID,
   REDIRECT_URI: 'https://musify-app.netlify.com',
   BASE_URL: 'https://api.spotify.com/v1',
   STORAGE_KEY: 'auth_token',
