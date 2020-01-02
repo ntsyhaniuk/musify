d5861d5f042df1de0602df4d683d32fe417690a8
nazartsyhanyuk <nazartsyhanyuk@gmail.com>
2020-01-02 18:35:02 +0200

    fix for localhost

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  CLIENT_ID: '',
+  CLIENT_ID: '', // client ID will be inserted automatically from environment variable
   REDIRECT_URI: 'https://musify-app.netlify.com',
   BASE_URL: 'https://api.spotify.com/v1',
   STORAGE_KEY: 'auth_token',
