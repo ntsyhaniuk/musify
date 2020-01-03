4497101737b1c2d9f17b988ca7ed61506a65d169
nazartsyhanyuk <nazartsyhanyuk@gmail.com>
2020-01-03 17:55:57 +0200

    fix protocol

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,10 @@ export const environment = {
   production: false,
   CLIENT_ID: '', // here should be your client id from registered application
   REDIRECT_URI: 'http://localhost:4300/',
-  BASE_URL: 'https://api.spotify.com/v1',
+  LASTFM_API_KEY: '',
   STORAGE_KEY: 'auth_token',
+  BASE_SPOTIFY_URL: 'https://api.spotify.com/v1',
+  BASE_LASTFM_URL: 'https://ws.audioscrobbler.com/2.0',
   AUTH_URL: 'https://accounts.spotify.com/authorize'
 };
 
