88087a3aa4b24fd772d8a7b972cd9b86a767b4b6
nazartsyhanyuk <nazartsyhanyuk@gmail.com>
2020-01-03 18:01:28 +0200

    fix protocol

--- src/config-env.js
+++ src/config-env.js
@@ -14,7 +14,7 @@ export const environment = {
   LASTFM_API_KEY: '${LASTFM_API_KEY}',
   STORAGE_KEY: 'auth_token',
   BASE_SPOTIFY_URL: 'https://api.spotify.com/v1',
-  BASE_LASTFM_URL: 'http://ws.audioscrobbler.com/2.0',
+  BASE_LASTFM_URL: 'https://ws.audioscrobbler.com/2.0',
   AUTH_URL: 'https://accounts.spotify.com/authorize'
 };
 `;
